0dd5fedebe38235c9e0ca4b353288d6cebc0280e
fadhi690mk <fadhilmancheri99@gmail.com>
2026-05-17 23:08:55 +0530

    Update ALLOWED_HOSTS in settings.py to include additional IP address

--- awamer/settings.py
+++ awamer/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get("SECRET_KEY", "django-insecure-dev-key-change-in-production")
 DEBUG = os.environ.get("DEBUG", "True").lower() in ("1", "true", "yes")
-ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="localhost,127.0.0.1").split(",")
+ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="localhost,127.0.0.1,187.77.189.11").split(",")
 
 INSTALLED_APPS = [
     "django.contrib.admin",
